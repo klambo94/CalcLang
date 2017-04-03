579a88415b492aeaf6ac92ad9fa836d092dce1f7
klambo94@gmail.com <klambo94@gmail.com>
2017-04-02 20:17:09 -0600

    Removed dead code in Statement(s) nodes

--- src/StatementNode.java
+++ src/StatementNode.java
@@ -10,42 +10,9 @@ public class StatementNode extends Node {
         super(kind, first);
     }
 
-   // public StatementNode (String kind, Node one, Node two) {
-     //   super(kind, one, two);
-  //  }
-
-
     @Override
     public void executeNode() throws Exception {
        this.first.executeNode();
     }
 
-//    @Override
-//    public void executeNode() throws Exception {
-//        String kind = this.kind;
-//
-//        switch(kind) {
-//            case "id":
-//                // When statement node created the first is the first operend and the second is
-//                // the second operend. The math symbol does not get passed
-//                new AssignmentNode(kind, first, second).executeNode();
-//                break;
-//            case "keywords":
-//                String info = this.info;
-//                if(info.equals("input")) {
-//                    new InputNode(kind, first, second).executeNode(); //first I believe is the
-//                    // input. We just need String V so those would be in the second and third
-//                    // children
-//                } else if(info.equals("msg") || info.equals("show")) {
-//                    new DisplayNode(kind, first).executeNode();
-//                } else if(info.equals("newline")){
-//                    System.out.println("");
-//                } else {
-//                    throw new Exception("Unrecognized keyword:" + info);
-//                }
-//                break;
-//            default:
-//                throw new Exception("Unrecognized kind: " + kind);
-//        }
-//    }
 }
--- src/StatementsNode.java
+++ src/StatementsNode.java
@@ -8,46 +8,12 @@ public class StatementsNode extends Node {
     }
 
     @Override
-    public void executeNode() throws  Exception {
-        if(first != null) {
+    public void executeNode () throws Exception {
+        if (first != null) {
             first.executeNode();
         }
-        if(second != null) {
+        if (second != null) {
             second.executeNode();
         }
     }
-
-
-//    @Override
-//    public void executeNode() throws Exception {
-//        String kind = this.kind;
-//
-//        switch(kind) {
-//            case "id":
-//                //first is the variable id, third is the digit.
-//                new StatementNode(kind, first, third).executeNode();
-//                break;
-//            case "keyword":
-//                String info = this.info;
-//                if(info.equals("input")) {
-//                    new StatementNode(kind, second, third).executeNode();
-//                } else if(info.equals("msg") || info.equals("show")) {
-//                    new StatementNode(kind, second);
-//                } else if(info.equals("newline")){
-//                    new StatementNode(new Token(kind, info));
-//                } else {
-//                    throw new Exception("Unrecognized keyword:" + info);
-//                }
-//                break;
-//            default:
-//                throw new Exception("Unrecognized kind: " + kind);
-//        }
-//    }
 }
-//TODO: Things to keep in mind while creating the parser
-//TODO: I've built this in a way that when the statemtntsNode is created you pass in each token.
-//TODO: So for the assignmentnode we don't pass in the '=' operator
-//TODO: For the (expression) factor we don't pass in the parens. - This needs to be taken care of
-// in Parser
-//TODO: For the input we only pass in the STRING V in the statement.
-//TODO: For MSG or Show we only pass in the STRING or V.
